ca6482704b291c7418972fc41b6308adb134fb02
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2024-08-30 15:22:48 +0200

    adds service name override

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
-  resource_prefix = "${var.project}-${var.environment}" # computed naming standard convention for all resources
+  resource_prefix = "${var.environment}-${var.project}"
 
-  full_service_name = "${local.resource_prefix}-${var.app_name}" # abc-prod-ssm-proxy
+  full_service_name = var.full_service_name != "" ? var.full_service_name : "${local.resource_prefix}-${var.app_name}"
 
   tags_app_module = merge(
     var.tags,         # Tags coming from calling TF
